411cc8fcdd9f17c10e8b0a0f83d5c155ea8d1fdf
sobhan <99309178+salari12@users.noreply.github.com>
2022-06-10 20:08:36 +0430

    update

--- country/java/centerjava.js
+++ country/java/centerjava.js
@@ -1,1 +1,9 @@
-document.getElementById('fontbig').style.fontSize="20px";
+function fontbig(){
+    document.getElementById('fontbig').style.fontSize="20px";
+}
+function f1(){
+    document.getElementById('f1').style.display='none';
+}
+function f2(){
+    document.getElementById('f2').style.display='none';
+}
