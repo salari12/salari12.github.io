b60555bfde0aa2a279ba1a945d104dc406a2b069
sobhan <99309178+salari12@users.noreply.github.com>
2022-06-18 20:38:46 +0430

    periodic table update

---
+++ periodic table/Periodic Table.js
@@ -0,0 +1,5 @@
+function background(){
+    document.getElementById("b1").style.backgroundColor='white';
+    document.getElementById('b2').style.color="black";
+    document.getElementById('b3').setAttribute("class", "demclass");
+}
