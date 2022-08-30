26d5142c92a7855bffa107f60f954577c64ee4b8
sobhan <99309178+salari12@users.noreply.github.com>
2022-08-30 20:18:10 +0430

    update

--- big project/project/gap/styles/private.js
+++
@@ -1,6 +0,0 @@
-function counting(){
-    var a =Number(document.getElementById("co1").value);
-    var b =Number(document.getElementById("co2").value);
-     document.getElementById("co3").value=a/b;
- }
- 
