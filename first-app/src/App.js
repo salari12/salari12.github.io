508cd09db8c1fb492a45eda406cca7432ff4f159
sobhan <99309178+salari12@users.noreply.github.com>
2024-11-23 19:01:41 +0330

    create

---
+++ first-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello <code>Welcome</code>To OUR community...
+        </p>
+        <h1>DELEVOPED BY SALARIⓒ</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
